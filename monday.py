cc0c1b46f5b141523c73ea7423e6b5b63c896d3f
KakeguriMashoBayou <boyul@nycstudents.net>
2026-04-27 09:25:08 -0400

    Create monday.py

---
+++ monday.py
@@ -0,0 +1,8 @@
+name = input("What is your pokemon name?: ")
+class Tamgaotchi: 
+     def __init__(self, name, health, hunger, status, energy):
+        self.name = name
+        self._health = health  
+        self._hunger = hunger        
+        self._status = status
+        self._energy = energy 
