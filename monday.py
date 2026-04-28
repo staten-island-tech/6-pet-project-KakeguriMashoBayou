ca515b8c060f8993015118629900db45aea76c7a
KakeguriMashoBayou <boyul@nycstudents.net>
2026-04-28 09:20:07 -0400

    Update monday.py

--- monday.py
+++ monday.py
@@ -1,8 +1,17 @@
 name = input("What is your pokemon name?: ")
-class Tamgaotchi: 
-     def __init__(self, name, health, hunger, status, energy):
-        self.name = name
-        self._health = health  
-        self._hunger = hunger        
-        self._status = status
-        self._energy = energy 
+health = 100
+status = ["paralysed", "poisoned", "burned", "frozen" ]
+
+class Pokemon: 
+   def __init__(self, name, health, hunger, status, energy):
+      self.name = name
+      self._health = health  
+      self._hunger = hunger        
+      self._status = status
+      self._energy = energy
+        
+   def status(self):
+      if self.__status == status:
+         self.__health -= 10
+         print(health)
+
