2e1508ca58609bf3c350fca4bf45ab5eae04532a
KakeguriMashoBayou <boyul@nycstudents.net>
2026-04-29 09:26:00 -0400

    Update monday.py

--- monday.py
+++ monday.py
@@ -1,17 +1,25 @@
 name = input("What is your pokemon name?: ")
-health = 100
 status = ["paralysed", "poisoned", "burned", "frozen" ]
 
 class Pokemon: 
    def __init__(self, name, health, hunger, status, energy):
       self.name = name
-      self._health = health  
-      self._hunger = hunger        
-      self._status = status
-      self._energy = energy
+      self.__health = health  
+      self.__hunger = hunger        
+      self.__status = status
+      self.__energy = energy
         
+ 
    def status(self):
       if self.__status == status:
-         self.__health -= 10
-         print(health)
+         self.__health -= 20
+         print(self.__health)
+         if self.__health < 60:
+            print(self.__health, "your pet is hurt")
+         elif self.__health < 30:
+            print("your pet needs immediete help")
+         else:
+            self.__health = 0
+            print(self.__health, "your pet is going to see your grandma")
+            
 
