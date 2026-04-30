7887ef0bfbc743654487f41c1cf374c7b079b185
KakeguriMashoBayou <boyul@nycstudents.net>
2026-04-30 09:13:18 -0400

    Update monday.py

--- monday.py
+++ monday.py
@@ -15,7 +15,6 @@ class Pokemon:
       if self.__status in status_list:
          self.__health -= 20
          print("health:", self.__health)
-     
       self.__health = max(0, min(100, self.__health))
       if self.__health <=0:
          print("your pokemon is dead, dig a grave for it")
@@ -25,6 +24,7 @@ class Pokemon:
          print("your pokemon has a bruise")
 
 random_status = random.choice(status_list)
+
 
 
 
