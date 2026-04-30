cfe4f44deabe788883674ae3c20aecffbcf0337a
KakeguriMashoBayou <boyul@nycstudents.net>
2026-04-30 09:10:35 -0400

    Update monday.py

--- monday.py
+++ monday.py
@@ -1,5 +1,6 @@
+import random
 name = input("What is your pokemon name?: ")
-status = ["paralysed", "poisoned", "burned", "frozen" ]
+status_list = ["paralysed", "poisoned", "burned", "frozen" ]
 
 class Pokemon: 
    def __init__(self, name, health, hunger, status, energy):
@@ -10,16 +11,23 @@ class Pokemon:
       self.__energy = energy
         
  
-   def status(self):
-      if self.__status == status:
+   def the_status(self):
+      if self.__status in status_list:
          self.__health -= 20
-         print(self.__health)
-         if self.__health < 60:
-            print(self.__health, "your pet is hurt")
-         elif self.__health < 30:
-            print("your pet needs immediete help")
-         else:
-            self.__health = 0
-            print(self.__health, "your pet is going to see your grandma")
+         print("health:", self.__health)
+     
+      self.__health = max(0, min(100, self.__health))
+      if self.__health <=0:
+         print("your pokemon is dead, dig a grave for it")
+      elif self.__health <30:
+         print("your pokemon is severly damaged")
+      elif self.__health <80:
+         print("your pokemon has a bruise")
+
+random_status = random.choice(status_list)
+
+
+
+
             
 
